2d56132487c7dc406c24b5aeacb8c803364cf8a5
JARED06J <garbanzopicadomichaeljared@gmail.com>
2026-04-14 16:15:46 -0600

    db.js

---
+++ backend/src/config/db.js
@@ -0,0 +1,22 @@
+// db.js
+const sql = require('mssql');
+
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER, // ejemplo: localhost
+    database: process.env.DB_NAME,
+    options: {
+        encrypt: false, // true si usas Azure
+        trustServerCertificate: true
+    }
+};
+
+const pool = new sql.ConnectionPool(config);
+const poolConnect = pool.connect();
+
+module.exports = {
+    sql,
+    pool,
+    poolConnect
+};
