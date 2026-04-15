52d9177256f6ccd88c67d8f11e97cb2c4f99039e
JARED06J <garbanzopicadomichaeljared@gmail.com>
2026-04-14 22:44:01 -0600

    Parte de Jared

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 // app.js
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 // rutas
 const matriculaRoutes = require('./src/modules/matricula/matricula.routes');
